84e378ff64281937652e90e3c4839ae409648290
djfw <dan@1xn.org>
2019-11-05 00:31:25 +0100

    fix

--- js/face.js
+++ js/face.js
@@ -4,11 +4,11 @@ var img = document.querySelector('.dot-tone-portrait img');
 new BreathingHalftone( img, {
   dotSize: 1/200,
   dotSizeThreshold: 0.01,
-  initVelocity: 0.9,
+  initVelocity: 0.6,
   oscPeriod: 2,
   channels: [ 'lum' ],
   friction: 0.25,
-  hoverDiameter: 0.8,
+  hoverDiameter: 0.3,
   hoverForce: 0.004,
   activeDiameter: -0.4,
   activeForce: 0.004
